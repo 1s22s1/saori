8054cad7ac3ade46296e1da78327a8d57ac6282c
1s22s1 <1s22s1@users.noreply.github.com>
2025-01-07 10:20:26 +0900

    :up:

---
+++ 20250107/APG4b_cm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    for (int i = 0; i < A; i++)
+    {
+        cout << "]";
+    }
+
+    cout << endl;
+
+    for (int i = 0; i < B; i++)
+    {
+        cout << "]";
+    }
+
+    cout << endl;
+}
